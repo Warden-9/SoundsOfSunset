6d11f97a4c2c0587ada2fea4a08c8625f96d0dae
unknown <hibbingm@gmail.com>
2022-01-15 14:43:01 -0700

    auth update

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Mark Hibbing CIS 345 Tues/Thurs 12:00 - 1:15pm Project Project Submission
 
+from lib2to3.pgen2.token import tok_name
 import discord
 from discord.ext import commands
 import music
+import json
+
+f = open('token.json')
+t = json.load(f)
 
 cogs = [music]
 
@@ -11,5 +16,5 @@ client = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("OTA5MTgyNTc5NjU4MzI2MTE3.YZAkQg.RaMqgDi3-yULTReu4wo_Smqi-FM")
+client.run(t)
 
